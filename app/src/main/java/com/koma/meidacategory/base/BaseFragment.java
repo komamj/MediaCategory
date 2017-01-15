9b4e46302d9667db9d5c4164cf80dc884b83fde4
koma <koma_mj@icloud.com>
2017-01-15 15:29:08 +0800

    init structure.

---
+++ app/src/main/java/com/koma/meidacategory/base/BaseFragment.java
@@ -0,0 +1,77 @@
+package com.koma.meidacategory.base;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.koma.meidacategory.util.LogUtils;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by koma on 2017/1/14.
+ */
+
+public abstract class BaseFragment extends Fragment {
+    private static final String TAG = BaseFragment.class.getSimpleName();
+
+    @Override public void onAttach(Context context){
+        super.onAttach(context);
+        LogUtils.i(TAG,"onAttach");
+    }
+
+    @Override public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        LogUtils.i(TAG,"onCreate");
+    }
+
+    @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        LogUtils.i(TAG,"onCreateView");
+        return createView(inflater,container,savedInstanceState);
+    }
+    public abstract View createView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState);
+
+    @Override public void onViewCreated(View view,Bundle savedInstanceState){
+        super.onViewCreated(view,savedInstanceState);
+        LogUtils.i(TAG,"onViewCreated");
+        ButterKnife.bind(this,view);
+    }
+
+    @Override public void onStart(){
+        super.onStart();
+        LogUtils.i(TAG,"onStart");
+    }
+
+    @Override public void onResume(){
+        super.onResume();
+        LogUtils.i(TAG,"onResume");
+    }
+
+    @Override public void onPause(){
+        super.onPause();
+        LogUtils.i(TAG,"onPause");
+    }
+
+    @Override public void onStop(){
+        super.onStop();
+        LogUtils.i(TAG,"onStop");
+    }
+
+    @Override public void onDestroyView(){
+        super.onDestroyView();
+        LogUtils.i(TAG,"onDestroyView");
+    }
+
+    @Override public void onDestroy(){
+        super.onDestroy();
+        LogUtils.i(TAG,"onDestroy");
+    }
+
+    @Override public void onDetach(){
+        super.onDetach();
+        LogUtils.i(TAG,"onDetach");
+    }
+}
---
+++ app/src/main/java/com/koma/meidacategory/base/BasePresenter.java
@@ -0,0 +1,10 @@
+package com.koma.meidacategory.base;
+
+/**
+ * Created by koma on 2017/1/14.
+ */
+
+public interface BasePresenter {
+    void subscribe();
+    void unSubscribe();
+}
